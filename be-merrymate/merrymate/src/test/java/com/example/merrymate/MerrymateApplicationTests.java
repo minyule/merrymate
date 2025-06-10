1d3a35944a70a47905d84fa6a95cda44829f4a13
minyule <1004home3@gmail.com>
2025-06-11 07:22:16 +0900

    first commit

---
+++ be-merrymate/merrymate/src/test/java/com/example/merrymate/MerrymateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.merrymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerrymateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
